168da1294700f4aba22f42939e9b5da2b10716a5
vusalzeynalovvz <112488906+vusalzeynalovvz@users.noreply.github.com>
2022-12-19 20:45:14 +0400

    19.12.2022

--- Final Project/app.js
+++ Final Project/app.js
@@ -230,6 +230,39 @@ $('.card-slider-2').slick({
      ]
 });
 
+// $('#last-one').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   slidesToScroll: 2,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//      ]
+// });
+
         
 
 let buttons = document.querySelectorAll(".links button");
